fcefdea86bfefa98cd30450b7c041b8e0d76a11b
melon <seazzw@126.com>
2019-01-29 16:55:35 +0800

    集成Fusion UI并按需引入

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import './global.scss'
+import './global.scss';
+import { Button } from '@alifd/next';
 
 class App extends Component {
     render() {
         return (
             <div>
                 Hello React
+                <Button type="primary">Normal</Button>
             </div>
         );
     }
