3d8bd2ec85aebd8d244fff98fc22dd7c901a39b2
melon <seazzw@126.com>
2019-02-27 19:01:12 +0800

    update component Login

--- src/layouts/BlankLayout/index.js
+++ src/layouts/BlankLayout/index.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Grid } from '@alifd/next';
+import Footer from './components/Footer';
+import Intro from './components/Intro';
+import './index.scss';
+
+const { Row, Col } = Grid;
 
 class BlankLayout extends Component {
     static propTypes = {
@@ -9,8 +15,18 @@ class BlankLayout extends Component {
     render() {
         const { children } = this.props;
         return (
-            <div style={{ width: '100%', height: '100%' }}>
-                {children}
+            <div className='blank'>
+                <Row wrap className='blank__row'>
+                    <Col l='12'>
+                        <Intro />
+                    </Col>
+                    <Col l='12'>
+                        <div className='blank__form'>
+                            {children}
+                        </div>
+                    </Col>
+                </Row>
+                <Footer />
             </div>
         );
     }
